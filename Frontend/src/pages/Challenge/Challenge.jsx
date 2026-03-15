992b9d6ba3079126b6a2802264a2d01073414c30
Leozheng04 <leozheng1204@gmail.com>
2026-03-15 13:00:08 -0400

    Improved leaderboard page to be more mobile responsive

--- Frontend/src/pages/Challenge/Challenge.jsx
+++ Frontend/src/pages/Challenge/Challenge.jsx
@@ -6,20 +6,19 @@ const Challenge = () => {
                 <h1 className = {styles["challenge-title"]}>Challenge</h1>
                 <p className = {styles["challenge-subtitle"]}>Complete challenges to earn bonus points and climb the leaderboard</p>
                 <section className = {styles["daily-challenge"]}>
-                    <div className={styles["challenge-type"]}>DAILY CHALLENGE</div>
+                    <div className={styles["challenge-type"]}>DAILY CHALLENGES</div>
                     
                 </section>
                 <section className = {styles["weekly-challenge"]}>
-                    <div className={styles["challenge-type"]}>WEEKLY CHALLENGE</div>
+                    <div className={styles["challenge-type"]}>WEEKLY CHALLENGES</div>
                     
                 </section>
                 <section className = {styles["special-challenge"]}>
-                    <div className={styles["challenge-type"]}>SPECIAL</div>
+                    <div className={styles["challenge-type"]}>SPECIAL CHALLENGES</div>
                     
                 </section>
             </div>
         </div>
-        
     )
 }
 export default Challenge;
